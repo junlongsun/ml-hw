f1a27ac8f068f0c1739d5202cac449df4c26db53
junlongsun <junlong.sun@gmail.com>
2015-10-08 22:37:24 -0600

    for 6 c and 2 kernel

--- svm/mytest2.py
+++ svm/mytest2.py
@@ -1,8 +1,32 @@
 import numpy as np
+from sklearn.svm import SVC
 
-a = np.array([[0,0]])
-a = np.append(a, np.array([[1,2]]), axis=0)
-a = np.append(a, np.array([[3,4]]), axis=0)
-print type(a)
-print a
-print a[1]
+
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+
+X = np.array([[-1, -1], [-2, -1], [2, 1], [1, 2], [0,2]])
+y = np.array([1, 1,  2, 2, 2])
+
+
+train_x  = [[-0.8, -1]]
+clf = SVC(C=1.0, kernel="linear")
+clf.fit(X, y)
+print(clf.predict(train_x))
+
+
+sv = clf.support_vectors_
+print sv
+print clf.support_
+#show(sv)
--- svm/scikit.py
+++ svm/scikit.py
@@ -3,6 +3,20 @@ import argparse
 import numpy as np
 from numpy import zeros, shape
 
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+
 class Numbers:
     """
     Class to store MNIST data
@@ -34,10 +48,19 @@ def select3and8(x,y):
             #new_x.append()
             #print "ok"
             if y[i] == 3:
-                new_y.append(1)
+                new_y.append(3)
             else:
-                new_y.append(0)
+                new_y.append(8)
     return new_x[1:], new_y
+
+def calcScore(prediction, result):
+    sum = 0
+    for i in range(len(prediction)):
+        if prediction[i] == result[i]:
+            sum += 1
+            #print sum
+    #print len(prediction)
+    return sum/float(len(prediction))
 
 parser = argparse.ArgumentParser(description='KNN classifier options')
 parser.add_argument('--k', type=int, default=3,
@@ -61,21 +84,79 @@ test_x, test_y = select3and8(data.test_x, data.test_y)
 #print shape(train_x)
 #print train_x[0:2]
 #print train_y[0:10]
+
 '''
-if args.limit > 0:
-    print("Data limit: %i" % args.limit)
-    knn = Knearest(data.train_x[:args.limit], data.train_y[:args.limit],
-                       args.k)
-else:
-    knn = Knearest(data.train_x, data.train_y, args.k)
-    print("Done loading data")
+clf0 = svm.SVC(C=10.0, kernel="linear")
+clf0.fit(train_x, train_y)
+print "c: 10.0, k: linear"
+print calcScore(clf0.predict(test_x), test_y)
+
+print clf0.support_
+print clf0.support_vectors_[0]
+
+numpix = 28
+bp = [numpix*i for i in range(numpix+1)]
+image = [clf0.support_vectors_[bp[i]:bp[i+1]] for i in range(numpix)]
+show(image)
 '''
 
-#X = [[0, 0], [1, 1]]
-#y = [0, 1]
-clf = svm.SVC()
-clf.fit(train_x, train_y)
-print clf.predict(test_x)
-#print clf.support_vectors_
-#print clf.support_
-#print clf.n_support_
+
+clf0 = svm.SVC(C=10.0, kernel="linear")
+clf0.fit(train_x, train_y)
+print "c: 10.0, k: linear"
+print calcScore(clf0.predict(test_x), test_y)
+
+clf1 = svm.SVC(C=1.0, kernel="linear")
+clf1.fit(train_x, train_y)
+print "c: 1.0, k: linear"
+print calcScore(clf1.predict(test_x), test_y)
+
+clf2  = svm.SVC(C=0.1, kernel="linear")
+clf2.fit(train_x, train_y)
+print "c: 0.1, k: linear"
+print calcScore(clf2.predict(test_x), test_y)
+
+clf3  = svm.SVC(C=0.01, kernel="linear")
+clf3.fit(train_x, train_y)
+print "c: 0.01, k: linear"
+print calcScore(clf3.predict(test_x), test_y)
+
+clf4  = svm.SVC(C=0.001, kernel="linear")
+clf4.fit(train_x, train_y)
+print "c: 0.001, k: linear"
+print calcScore(clf4.predict(test_x), test_y)
+
+clf5  = svm.SVC(C=0.0001, kernel="linear")
+clf5.fit(train_x, train_y)
+print "c: 0.0001, k: linear"
+print calcScore(clf5.predict(test_x), test_y)
+
+clf6  = svm.SVC(C=10, kernel="rbf")
+clf6.fit(train_x, train_y)
+print "c: 10.0, k: rbf"
+print calcScore(clf6.predict(test_x), test_y)
+
+clf7  = svm.SVC(C=1, kernel="rbf")
+clf7.fit(train_x, train_y)
+print "c: 1.0, k: rbf"
+print calcScore(clf7.predict(test_x), test_y)
+
+clf8  = svm.SVC(C=0.1, kernel="rbf")
+clf8.fit(train_x, train_y)
+print "c: 0.1, k: rbf"
+print calcScore(clf8.predict(test_x), test_y)
+
+clf9  = svm.SVC(C=0.01, kernel="rbf")
+clf9.fit(train_x, train_y)
+print "c: .01, k: rbf"
+print calcScore(clf9.predict(test_x), test_y)
+
+clf10  = svm.SVC(C=0.001, kernel="rbf")
+clf10.fit(train_x, train_y)
+print "c: 0.001, k: rbf"
+print calcScore(clf10.predict(test_x), test_y)
+
+clf11  = svm.SVC(C=0.0001, kernel="rbf")
+clf11.fit(train_x, train_y)
+print "c: .0001, k: rbf"
+print calcScore(clf11.predict(test_x), test_y)
